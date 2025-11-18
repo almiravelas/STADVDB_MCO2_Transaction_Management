08b2fc3989760096459a8c57b1735cb0aff5a5d9
Minwicorn <149855396+Minwicorn@users.noreply.github.com>
2025-11-19 07:14:21 +0800

    added: file setup

---
+++ models/node.js
@@ -0,0 +1,5 @@
+// conceptual code of the database connections (Node.js)
+
+/* const db1 = mysql.createConnection({ }); // Central
+/* const db2 = mysql.createConnection({ }); // Fragment 1
+/* const db3 = mysql.createConnection({ }); // Fragment 2 */
